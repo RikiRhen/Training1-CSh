fe999acfde922ff75aeb6a515d39e8285e073d50
RikiRhen <riki.rhen@live.se>
2024-05-20 15:06:14 +0200

    Teoretisk kod färdig. Påbörjar testning.

--- Program.cs
+++ Program.cs
@@ -4,7 +4,80 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            List<Anställd> anställda = new List<Anställd>();
+            bool running = true;
+
+            while (running)
+            {
+                Console.WriteLine("Välj ett alternativ:");
+                Console.WriteLine("1. Lägg till en ny anställd");
+                Console.WriteLine("2. Visa samtliga anställda");
+                Console.WriteLine("3. Avsluta");
+                string val = Console.ReadLine();
+                int anstNr = 0000;
+
+                switch (val)
+                {
+                    case "1":
+                        Console.Write("Förnamn?: ");
+                        string fNamn = Console.ReadLine();
+
+                        Console.Write("Efternamn?: ");
+                        string eNamn = Console.ReadLine();
+
+                        Console.Write("Lön?: ");
+                        int lön;
+                        while (!int.TryParse(Console.ReadLine(), out lön))
+                        {
+                            Console.Write("Ogiltig lön, vänligen håll till heltal.");
+                        }
+                        anstNr++;
+
+                        anställda.Add(new Anställd(fNamn, eNamn, anstNr, lön));
+                        break;
+
+                    case "2":
+                        foreach (Anställd anställd in anställda)
+                        {
+                            Console.WriteLine(anställd.ToString());
+                        }
+                        break;
+
+                    case "3":
+                        running = false;
+                        break;
+
+                    default:
+                        Console.WriteLine("Ogiltigt val. Vänligen försök igen!\n");
+                        break;
+
+                }
+            }
+
         }
+    }
+
+    public class Anställd
+    {
+        public string fNamn { get; set; }
+        public string eNamn { get; set; }
+        public int anstNr { get; set; }
+        public int lön { get; set; }
+
+
+        public Anställd(string fNnamn, string eNnamn, int anstNr, int lön)
+        {
+            this.fNamn = fNamn;
+            this.eNamn = eNamn;
+            this.anstNr = anstNr;
+            this.lön = lön;
+
+        }
+
+        public string toString()
+        {
+            return $"{fNamn} {eNamn}, Anställningsnummer: {anstNr}, Lön {lön}";
+        }
+
     }
 }
